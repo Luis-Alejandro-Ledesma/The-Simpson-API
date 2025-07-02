f41bc067a9915369f690570fe1bd35e3e6e2122e
araceli4920 <araceliicarri4920@gmail.com>
2025-07-01 19:34:36 -0500

    tarjetas dinamicas

--- Js/main.js
+++ Js/main.js
@@ -1,4 +1,6 @@
 const container = document.getElementById("characters-container");
+let currentPage = 1;
+const limit = 50;
 
 function renderCharacterCard(character) {
   return `
@@ -15,3 +17,22 @@ function verDetalles(nombre) {
   const nombreParam = encodeURIComponent(nombre);
   window.location.href = `character.html?nombre=${nombreParam}`;
 }
+
+function loadCharacters(page = 1) {
+  const API_URL = `https://apisimpsons.fly.dev/api/personajes?limit=${limit}&page=${page}`;
+  fetch(API_URL)
+    .then(response => response.json())
+    .then(data => {
+      const personajes = data.docs;
+      container.innerHTML = "";
+      personajes.forEach(character => {
+        container.innerHTML += renderCharacterCard(character);
+      });
+    })
+    .catch(error => {
+      console.error("Error al cargar personajes:", error);
+      container.innerHTML = "<p>Error al cargar los personajes.</p>";
+    });
+}
+
+loadCharacters();
